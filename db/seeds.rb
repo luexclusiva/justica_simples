34551c52a78d2ff7efa47977e5eae9d714432f98
Joao Baumotte <jbaumotte@gmail.com>
2020-12-11 12:24:05 -0300

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,56 +6,56 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.destroy_all
-#
-# user = User.create(
-#   email: 'admin@admin.com',
-#   first_name: 'Admin',
-#   last_name: 'Admin',
-#   password: "123456",
-#   password_confirmation: "123456"
-# )
-# # file = URI.open(Faker::Avatar.image)
-# # user.photo.attach(io: file, filename: "#{user.username}.png", content_type: 'image/png')
-#
-# response = Net::HTTP.get(URI("https://randomuser.me/api/?results=20&nat=BR"))
-#
-# datas = JSON.parse(response)
-# datas["results"].each_with_index do |data, index|
-#   user = User.create(
-#     email: data["email"],
-#     first_name: data["name"]["first"].capitalize,
-#     last_name: data["name"]["last"].capitalize,
-#     password: "123456",
-#     password_confirmation: "123456"
-#   )
-#   # file = URI.open(data["picture"]["large"])
-#   # user.photo.attach(io: file, filename: "#{user.username}.png", content_type: 'image/png')
-#   puts "#{index} Users have been created" if index % 5 == 0
-# end
-#
-# puts "All user created!"
-#
-# ## Populating Steps table #################################################################
-# require 'csv'
-#
-# file = "app/assets/csv/movimentos-jec.csv"
-#
-# csv_options = {
-#   col_sep: ',',
-#   quote_char: '"',
-#   headers: :first_row
-# }
-#
-#
-# CSV.foreach(file, csv_options) do |row|
-#   Step.create(
-#     cnj_number: row[0],
-#     integer: row[3],
-#     description: row[1],
-#     translation: row[2]
-#   )
-# end
+User.destroy_all
+
+user = User.create(
+  email: 'admin@admin.com',
+  first_name: 'Admin',
+  last_name: 'Admin',
+  password: "123456",
+  password_confirmation: "123456"
+)
+# file = URI.open(Faker::Avatar.image)
+# user.photo.attach(io: file, filename: "#{user.username}.png", content_type: 'image/png')
+
+response = Net::HTTP.get(URI("https://randomuser.me/api/?results=20&nat=BR"))
+
+datas = JSON.parse(response)
+datas["results"].each_with_index do |data, index|
+  user = User.create(
+    email: data["email"],
+    first_name: data["name"]["first"].capitalize,
+    last_name: data["name"]["last"].capitalize,
+    password: "123456",
+    password_confirmation: "123456"
+  )
+  # file = URI.open(data["picture"]["large"])
+  # user.photo.attach(io: file, filename: "#{user.username}.png", content_type: 'image/png')
+  puts "#{index} Users have been created" if index % 5 == 0
+end
+
+puts "All user created!"
+
+## Populating Steps table #################################################################
+require 'csv'
+
+file = "app/assets/csv/movimentos-jec.csv"
+
+csv_options = {
+  col_sep: ',',
+  quote_char: '"',
+  headers: :first_row
+}
+
+
+CSV.foreach(file, csv_options) do |row|
+  Step.create(
+    cnj_number: row[0],
+    integer: row[3],
+    description: row[1],
+    translation: row[2]
+  )
+end
 
 ######### Testing with one judicial ############################################
 require "json"
@@ -63,7 +63,6 @@ file = File.read("app/assets/judicials/processo_teste_2.json")
 
 
 judicial = JSON.parse(file)
-
 
 
 new_judicial = Judicial.create(
