6ecf11579b10b560065a8a80b07b6e98299c792b
Joao Baumotte <jbaumotte@gmail.com>
2020-12-16 11:53:36 -0300

    Moved and centralized subtitle of inicial page

--- app/javascript/lib/init_typed.js
+++ app/javascript/lib/init_typed.js
@@ -11,11 +11,12 @@ const initTyped = () => {
     var options = {
       strings: ['<p>Entenda seu processo e <strong>garanta seu Direito</strong></p>'],
       typeSpeed: 50,
-      showCursor: false,
-      loop: false,
+      loop: true,
       contentType: 'html',
       fadeOut: false,
-      startDelay: 20
+      startDelay: 20,
+      backDelay: 10000,
+      showCursor:false
     };
 
     new Typed('#banner-typed-text', options );
