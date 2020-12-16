3950541ed28970450272aeba9d97d1191fafe7cf
Eluzai Santos <luexclusiva@gmail.com>
2020-12-16 19:19:25 -0300

    Alteracoes visuais; codigo do botao Acompanhar: se nao estiver logado, usuario loga e eh redirecionado para a ultima pagina do sistema visitada antes do login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,29 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :persist_last_visited_path, :authenticate_user!
 
   def after_sign_in_path_for(current_user)
-    if current_user.user_judicials?
-      stored_location_for(current_user) || user_judicials_path(current_user)
+
+    # Após logar, verifica a última página visitada pelo usuário e 
+    # direciona de acordo com a situação.
+    if session[:last_visited_path].present?
+      session[:last_visited_path]
     else
-      stored_location_for(current_user) || root_path
+      root_path
     end
+
+    # if current_user.user_judicials?
+    #   user_judicials_path(current_user)
+    # else
+    #   root_path
+    # end
+
   end
+
+  # Pegar o path da página atual e defini-la como última página visitada.
+  def persist_last_visited_path
+    unless Rails.configuration.ignored_paths.include?(request.path) || request.xhr?
+      session[:last_visited_path] = request.path
+    end  
+  end
+
 end
